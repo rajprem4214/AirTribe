8e5571a58c3063033e57366be7a916b4d2b9f28c
rajprem4214 <rajprem4214@gmail.com>
2024-03-10 02:35:24 +0530

    feat(api): Add Lead Search API

--- src/api/v1/lead/handler.ts
+++ src/api/v1/lead/handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { AppDataSource } from "../../../data-source"
+import { ILike } from 'typeorm';
 import { Lead } from '../../../entity/Lead';
 
 export const createLeadHandler = async (req: Request, res: Response): Promise<void> => {
@@ -29,6 +30,55 @@ export const createLeadHandler = async (req: Request, res: Response): Promise<vo
         res.status(201).json(lead);
     } catch (error) {
         console.error('Error registering for course:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+export const updateLeadStatus = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id: number = parseInt(req.params.id);
+        const { status } = req.body;
+
+        // Retrieve the lead from the database
+        const leadRepository = AppDataSource.getRepository(Lead);
+        const lead = await leadRepository.findOneBy({ id });
+
+        if (!lead) {
+            res.status(404).json({ error: 'Lead not found' });
+            return;
+        }
+
+        if (status !== undefined) {
+            lead.status = status;
+        }
+
+
+        // Save the updated lead back to the database
+        await leadRepository.save(lead);
+
+        res.status(200).json(lead);
+    } catch (error) {
+        console.error('Error updating lead status:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+export const searchLeads = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { query } = req.query;
+
+        // Retrieve leads from the database based on name or email
+        const leadRepository = AppDataSource.getRepository(Lead);
+        const leads = await leadRepository
+            .createQueryBuilder('lead')
+            .where('lead.name ILIKE :query OR lead.email ILIKE :query', { query: `%${query}%` })
+            .cache(true) // Enable query caching
+            .getMany();
+
+        res.status(200).json(leads);
+    } catch (error) {
+        console.error('Error searching leads:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
 };
--- src/api/v1/lead/route.ts
+++ src/api/v1/lead/route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { createLeadHandler } from './handler';
+import { createLeadHandler, searchLeads, updateLeadStatus } from './handler';
 
 
 const leadRouter = Router();
 leadRouter.post('/new', createLeadHandler);
+leadRouter.patch('/:id', updateLeadStatus);
+leadRouter.get('/search', searchLeads);
 
 export { leadRouter };
