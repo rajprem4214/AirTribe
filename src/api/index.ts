8cb489c6551776544f285dc22f76bce90f5159bb
rajprem4214 <rajprem4214@gmail.com>
2024-03-10 16:04:17 +0530

    fix(api): Add all routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import * as express from "express";
 import { courseRouter } from "./v1/courses/routes";
 import { instructorRouter } from "./v1/instructors/routes";
 import { leadRouter } from "./v1/lead/route";
+import { commentRouter } from "./v1/comment/route";
 
 const apiV1Router = express.Router();
 
 apiV1Router.use("/courses", courseRouter);
 apiV1Router.use("/instructor", instructorRouter);
 apiV1Router.use("/lead", leadRouter);
+apiV1Router.use("/comment", commentRouter);
 
 export { apiV1Router };
